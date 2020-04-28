196dfcf97a40a8eba7a9023fe4bb9385e80571ab
ShahinGH <ghaneishahin73@gmail.com>
2020-04-28 16:59:03 +0200

    last commit

--- asset/js/costum.js
+++ asset/js/costum.js
@@ -1,11 +1,18 @@
 var myApp = new Vue({
   el: "#app",
   data: {
-    title: "Hello from Vue.js"
+    title: "Hello from Vue.js",
+    link: "http://toplearn.com"
   },
   methods: {
     changeTitle: function (event) {
       this.title = event.target.value;
+    },
+    sayHello: function () {
+      return "Hello World";
+    },
+    sayBye: function () {
+      return this.title;
     }
   }
 });
